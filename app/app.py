06f9264d334ec6bbeb79c7c8321b645c872593e9
Alexandre Andrade <aaca91@gmail.com>
2023-03-07 10:04:30 -0300

    changes on app.py - adding more stuff

--- app/app.py
+++ app/app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
-st.write('Welcome to my app')
+import numpy as np
+import pandas as pd
+
+st.markdown("""# My First Streamlit app
+## Use the slider to see the dataframe
+Try it out!""")
+
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
+
+st.checkbox('Check me, please!')
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
